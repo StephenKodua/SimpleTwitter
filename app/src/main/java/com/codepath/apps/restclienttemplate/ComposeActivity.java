52d0d92d8ca753ff1a1851b58bc3e2b6c3be1ad4
StephenKodua <koduaste@msu.edu>
2022-06-16 11:12:58 -0700

    Making some changes

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -47,6 +47,7 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        //hello
 
         client = TwitterApp.getRestClient(this);
 
